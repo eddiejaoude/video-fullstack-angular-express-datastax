de44770e232d4d62d900d59138d325062c32868e
Eddie Jaoude <eddie@jaoudestudios.com>
2021-09-04 10:56:44 +0100

    feat: material styling list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
